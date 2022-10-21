9d8c0708a07c52f6ef7dace0b335c2a0c4fc890e
William Young <wifryo@gmail.com>
2022-10-21 15:20:32 +0200

    Attempt connect.ts fix for deploy

--- database/connect.ts
+++ database/connect.ts
@@ -3,7 +3,7 @@ import postgres from 'postgres';
 
 // This loads all environment variables from a .env file
 // for all code after this line
-if (!process.env.FLY_IO_BUILD) config();
+if (process.env.NODE_ENV !== 'production') config();
 
 // Type needed for the connection function below
 declare module globalThis {
